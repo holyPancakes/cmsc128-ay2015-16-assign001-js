0eb86658c9b67751638bd419a59477e48c03bda7
holyPancakes <marubaul22@gmail.com>
2016-02-14 10:18:02 +0800

    initial push

---
+++ numberLibrary.js
@@ -0,0 +1,35 @@
+/**
+ *
+ *Accepts a whole number from zero (0) to 1 million (1000000; without commas for example: 1,000,000) and prints on screen the number in word form
+ *
+ */
+function numberToWords(){
+    
+}
+
+/**
+ *
+ *Accepts a number in word form (from zero to 1 million) and returns it in numerical form
+ *
+ */
+function wordsToNum(){
+    
+}
+
+/**
+ *
+ *Accepts two arguments: the first argument is the number in word form (from zero to 1 million) and the second argument is any of the following: JPY, PHP, USD. The function returns the number in words to its numerical form with a prefix of the currency
+ *
+ */
+function wordsToCurrency(){
+    
+}
+
+/**
+ *
+ *Accepts three arguments: the first is the number from zero to 1 miliion, the second is the delimiter to be used (single character only) and third, the number of jumps when the delimiter will appear (from right most going to left most digit) 
+ *
+ */
+function numberToWords(){
+    
+}
